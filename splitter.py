c55868efd47da0a2499f7abcb7e71cdebefc6c6e
imflikk <58894272+imflikk@users.noreply.github.com>
2021-04-04 19:44:57 -0700

    Added some examples and argument checks

--- splitter.py
+++ splitter.py
@@ -2,8 +2,26 @@ import json
 import codecs
 import sys
 
+
+if len(sys.argv) != 4:
+	print("\tUsage: \t\tpython3 splitter.py <input file> <num of objects per file> <data type>")
+	print("\n\tExample: \tpython3 splitter.py bloodhound-users.json 10000 users")
+	exit()
+
 input_file = sys.argv[1]
 obj_size = int(sys.argv[2])
+type_of_data = sys.argv[3]
+possible_data_types = ["users", "groups", "ous", "computers", "gpos", "domains"]
+
+if type_of_data not in possible_data_types:
+	print("Invalid Bloodhound data type.  Please choose one of the following types: ")
+	
+	for data_type in possible_data_types:
+		print("\t-" + data_type)
+	
+	exit()
+
+
 count = 1
 file_num = 1
 
@@ -11,7 +29,7 @@ with open(input_file, 'r', encoding='utf-8-sig') as f:
 	
 	data = json.load(f)
 	objects_left = True
-	num_objects_left = len(data['users']) # change this if parsing another piece of data
+	num_objects_left = len(data[type_of_data])
 
 	while objects_left:
 		print("[+] - " + str(num_objects_left) + " total objects left in the given file.\n")
@@ -19,14 +37,14 @@ with open(input_file, 'r', encoding='utf-8-sig') as f:
 		if (num_objects_left - obj_size) > 0:
 			print("Writing to file #" + str(file_num) + "...\n")
 
-			tmp_data = {'users': []}
+			tmp_data = {type_of_data: []}
 	
-			tmp_data['users'] = data['users'][obj_size * (count - 1):obj_size * count]
-			tmp_data['meta'] = {"count": len(tmp_data['users']), "type": "users", "version": 3}
+			tmp_data[type_of_data] = data[type_of_data][obj_size * (count - 1):obj_size * count]
+			tmp_data['meta'] = {"count": len(tmp_data[type_of_data]), "type": type_of_data, "version": 3}
 
 			new_json_data = json.dumps(tmp_data)
 
-			with open('./output'+ str(file_num) +'.json', 'w') as f:
+			with open('./output-'+ str(file_num) +'.json', 'w') as f:
 				f.write(new_json_data)
 			
 			num_objects_left -= obj_size
@@ -35,14 +53,14 @@ with open(input_file, 'r', encoding='utf-8-sig') as f:
 		else:
 			print("Writing to last file, #" + str(file_num) + "...\n")
 
-			tmp_data = {'users': []}
+			tmp_data = {type_of_data: []}
 	
-			tmp_data['users'] = data['users'][obj_size * (count - 1):]
-			tmp_data['meta'] = {"count": len(tmp_data['users']), "type": "users", "version": 3}
+			tmp_data[type_of_data] = data[type_of_data][obj_size * (count - 1):]
+			tmp_data['meta'] = {"count": len(tmp_data[type_of_data]), "type": type_of_data, "version": 3}
 
 			new_json_data = json.dumps(tmp_data)
 
-			with open('./output'+ str(file_num) +'.json', 'w') as f:
+			with open('./output-'+ str(file_num) +'.json', 'w') as f:
 				f.write(new_json_data)
 
 			objects_left = False
